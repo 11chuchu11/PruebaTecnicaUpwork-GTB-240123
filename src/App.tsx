fa7a69e1660e5c3870b8f27de573242c7ca467ad
Franco Veron Peralta <francoveronperalta@gmail.com>
2024-01-24 15:18:13 -0300

    canmbio de header

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { ContextProvider } from './Context'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import NotFound from './Views/NotFound'
+import { Page } from './Types'
+import { useEffect, useState } from 'react'
 
 const router = createBrowserRouter([
 	{
@@ -21,13 +23,20 @@ const router = createBrowserRouter([
 ]) 
 
 function App() {
+
+	const [page, setPage] = useState<Page>()
+	useEffect(() => {
+		setPage(() => pages.find(p => p.link === window.location.pathname))
+	}, [])
+
+	
 	return (
 		<ContextProvider>
 			<div className="flex h-full">
 				<NavBar navbarItems={pages} />
 
 				<div className="w-full m-5">
-					<Header page={pages[0]} />
+					<Header page={page ? page : pages[0]} />
 
 					<main className="mt-5 relative">
 						<RouterProvider router={router}/>
@@ -39,6 +48,7 @@ function App() {
 }
 
 export default App
+
 
 
 
